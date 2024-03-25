0e339d7ffb71a19c2b303b9bf1ec66ff83c05ad6
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-03-25 17:57:51 +0100

    finished exercises 1 to 10

---
+++ .github/script.js
@@ -0,0 +1,59 @@
+// Números:
+
+// 1. Escribe una función que tome dos números como argumentos y devuelva su suma.
+
+function suma(num1, num2) {
+  return num1 + num2;
+}
+
+// 2. Escribe una función que tome dos números como argumentos y devuelva su resta.
+
+function resta(num1, num2) {
+  return num1 + num2;
+}
+// 3. Escribe una función que tome dos números como argumentos y devuelva su producto.
+
+function producto(num1, num2) {
+  return num1 + num2;
+}
+// 4. Escribe una función que tome dos números como argumentos y devuelva su división.
+
+function division(num1, num2) {
+  return num1 + num2;
+}
+
+// 5. Escribe una función que tome un número base y un exponente y devuelva el resultado de elevar el número base a la potencia dada.
+
+function potencia(numBase, exponente) {
+  return Math.pow(numBase, exponente);
+}
+
+// 6. Escribe una función que tome dos números como argumentos y devuelva el resto de la división del primer número entre el segundo.
+
+function restoDivision(dividendo, divisor) {
+  return dividendo % divisor;
+}
+
+// 7. Escribe una función que tome un número como argumento y devuelva su raíz cuadrada.
+
+function raizCuadrada(num) {
+  return Math.sqrt(num);
+}
+
+// 8. Escribe una función que tome un número como argumento y devuelva su valor absoluto.
+
+function valorAbsoluto(num) {
+  return Math.abs(num);
+}
+
+// 9. Escribe una función que tome un número decimal como argumento y devuelva el número redondeado al entero más cercano.
+
+function numeroRedondeado(num) {
+  return Math.round(num);
+}
+
+// 10. Escribe una función que genere y devuelva un número aleatorio entre 0 y 1.
+
+function numeroRandom(num) {
+  Math.random(num);
+}
